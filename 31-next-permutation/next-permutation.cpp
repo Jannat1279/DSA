36aa3e9876b9ef98a28167d723ee76b51e35f30e
Jannat1279 <114873167+Jannat1279@users.noreply.github.com>
2025-06-30 11:57:21 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (33.05%) - LeetSync

---
+++ 31-next-permutation/next-permutation.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int i = nums.size() - 2;
+
+        // Step 1: Find first decreasing element from right
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+        if (i >= 0) {
+            int j = nums.size() - 1;
+
+            // Step 2: Find just larger element on right and swap
+            while (j >= 0 && nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums[i], nums[j]);
+        }
+
+        // Step 3: Reverse the right half
+        reverse(nums.begin() + i + 1, nums.end());
+    }
+};
