82638213d3b3f92ac52f8f4d8e1b86e4ca4f90e8
Jannat1279 <114873167+Jannat1279@users.noreply.github.com>
2025-06-30 12:32:13 +0530

    Update best-time-to-buy-and-sell-stock.cpp
    2 solutions

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -10,3 +10,17 @@ public:
         return profit;
     }
 };
+
+    int maxProfit(vector<int>& prices) {
+        int minPrice=INT_MAX;
+        int Profit=0;
+        for(int price:prices){
+            if(price<minPrice){
+                minPrice=price;
+            }
+            if(price-minPrice>Profit){
+                Profit=price-minPrice;
+            }
+        }
+        return Profit;
+    }
