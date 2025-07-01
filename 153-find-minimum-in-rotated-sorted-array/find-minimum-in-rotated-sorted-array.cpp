954f117c995d3b2726165191f440b3e4e3c1590b
Jannat1279 <114873167+Jannat1279@users.noreply.github.com>
2025-07-01 12:08:57 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (48.94%) - LeetSync

---
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int low = 0, high = nums.size() - 1;
+        int ans = INT_MAX;
+
+        // Perform binary search to find the minimum element in a rotated sorted
+        // array
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            // If the left half [low...mid] is sorted
+            if (nums[low] <= nums[mid]) {
+                // The smallest element in the left half is nums[low]
+                ans = min(ans, nums[low]);
+
+                // Eliminate the left half since the minimum cannot lie before
+                // mid (inclusive)
+                low = mid + 1;
+            } else {
+                // If right half [mid...high] is sorted, then nums[mid] could be
+                // the minimum
+                ans = min(ans, nums[mid]);
+
+                // Eliminate the right half since the minimum must lie to the
+                // left of mid
+                high = mid - 1;
+            }
+        }
+
+        return ans; // Return the minimum element found
+    }
+};
