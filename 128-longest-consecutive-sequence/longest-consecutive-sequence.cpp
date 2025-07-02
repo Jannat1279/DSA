d1d1925854f1caba4b4421ca210e1b0e5d2ab621
Jannat1279 <114873167+Jannat1279@users.noreply.github.com>
2025-07-02 12:30:09 +0530

    Update longest-consecutive-sequence.cpp
    With example

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -24,3 +24,46 @@ public:
         
     }
 };
+/*
+nums = [100, 4, 200, 1, 3, 2]
+st = {1, 2, 3, 4, 100, 200}
+
+Step : Iterate through the set
+For i = 1
+st.count(0) → false ⇒ valid start of sequence
+
+len = 1
+
+st.count(2) → true ⇒ len = 2
+
+st.count(3) → true ⇒ len = 3
+
+st.count(4) → true ⇒ len = 4
+
+st.count(5) → false ⇒ stop
+
+maxi = max(0, 4) = 4 ✅
+
+For i = 2
+st.count(1) → true ⇒ skip (not start of sequence)
+
+For i = 3
+st.count(2) → true ⇒ skip
+
+For i = 4
+st.count(3) → true ⇒ skip
+
+For i = 100
+st.count(99) → false ⇒ valid start
+
+len = 1, st.count(101) → false ⇒ stop
+
+maxi = max(4, 1) = 4
+
+For i = 200
+st.count(199) → false ⇒ valid start
+
+len = 1, st.count(201) → false ⇒ stop
+
+maxi = max(4, 1) = 4
+*/
