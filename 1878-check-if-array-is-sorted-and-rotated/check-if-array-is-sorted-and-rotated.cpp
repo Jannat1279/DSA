8e7ab1d23ee3c59c909128712ab53886d199f149
Jannat1279 <114873167+Jannat1279@users.noreply.github.com>
2025-07-02 11:31:29 +0530

    Update check-if-array-is-sorted-and-rotated.cpp
    With example

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -11,3 +11,14 @@ public:
         return (count<=1);
     }
 };
+
+/*
+nums[]=[2, 1, 3, 4]
+
+nums[0] > nums[1] → 2 > 1 ✅
+nums[1] > nums[2] → 1 > 3 ❌
+nums[2] > nums[3] → 3 > 4 ❌
+nums[3] > nums[0] → 4 > 2 ✅
+
+count=2, not rotated and sorted.
+*/
