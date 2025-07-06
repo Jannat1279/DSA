103f4ee2f4103d5c3a1f591cfd329ba6b1c4188d
Jannat1279 <114873167+Jannat1279@users.noreply.github.com>
2025-07-06 23:40:19 +0530

    Update move-zeroes.cpp
    With example

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int left=0, right=0;
+        int left=0;
         for(int right=0;right<nums.size();right++){
             if(nums[right]!=0){
                 // swap
@@ -16,3 +16,22 @@ public:
 };
 // TC - O(n)
 // SC - O(n)
+
+/*
+Example:
+vector<int> nums = {0, 1, 0, 3, 12};
+
+| Step | `right` | `left` | `nums`           | Action               |
+| ---- | ------- | ------ | ---------------- | -------------------- |
+| 1    | 0       | 0      | {0, 1, 0, 3, 12} | nums\[0] is 0 → skip |
+| 2    | 1       | 0      | {1, 0, 0, 3, 12} | Swap 1 with 0        |
+| 3    | 2       | 1      | {1, 0, 0, 3, 12} | nums\[2] is 0 → skip |
+| 4    | 3       | 1      | {1, 3, 0, 0, 12} | Swap 3 with 0        |
+| 5    | 4       | 2      | {1, 3, 12, 0, 0} | Swap 12 with 0       |
+
+✅ Final Output:
+
+nums = {1, 3, 12, 0, 0};
+
+
+*/
