07ccad395a123b92a26ef074605000e503f61002
Jannat1279 <114873167+Jannat1279@users.noreply.github.com>
2025-07-07 12:56:18 +0530

    Time: 6 ms (90.63%) | Memory: 12 MB (23.45%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -8,15 +8,31 @@
  */
 class Solution {
 public:
-    bool hasCycle(ListNode *head) {
-        set<ListNode*> sLL;
-        ListNode *curr=head;
-        while(curr!=NULL){
-            if(sLL.contains(curr))return true;
-            sLL.insert(curr);
-            curr=curr->next;
-        }
+    // bool hasCycle(ListNode *head) {
+    //     set<ListNode*> sLL;
+    //     ListNode *curr=head;
+    //     while(curr!=NULL){
+    //         if(sLL.contains(curr))return true;
+    //         sLL.insert(curr);
+    //         curr=curr->next;
+    //     }
         
-        return false;
+    //     return false;
+    // }
+
+    bool hasCycle(ListNode *head) {
+        ListNode* slow = head; // Tortoise
+        ListNode* fast = head; // Hare
+
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;           // Move 1 step
+            fast = fast->next->next;     // Move 2 steps
+
+            if (slow == fast) {
+                return true;             // Cycle detected
+            }
+        }
+
+        return false; // No cycle
     }
 };
