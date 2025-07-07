e91a06e2eadcdd005655b3a124af86f067443eba
Jannat1279 <114873167+Jannat1279@users.noreply.github.com>
2025-07-07 13:20:34 +0530

    Time: 5 ms (83.82%) | Memory: 11.4 MB (53.62%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -8,23 +8,48 @@
  */
 class Solution {
 public:
-    ListNode* detectCycle(ListNode* head) {
-        unordered_set<ListNode*> visited;
-        ListNode* current = head;
+//     ListNode* detectCycle(ListNode* head) {
+//         unordered_set<ListNode*> visited;
+//         ListNode* current = head;
 
-        // Traverse the list
-        while (current != nullptr) {
-            // If we've seen this node before, it's the start of the cycle
-            if (visited.find(current) != visited.end()) {
-                return current;
+//         // Traverse the list
+//         while (current != nullptr) {
+//             // If we've seen this node before, it's the start of the cyclei
+//             if (visited.find(current) != visited.end()) {
+//                 return current;
+//             }
+
+//             // Mark this node as visited
+//             visited.insert(current);
+//             current = current->next;
+//         }
+
+//         // If we reach here, there is no cycle
+//         return nullptr;
+//     }
+
+ListNode *detectCycle(ListNode *head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        // Phase 1: Detect cycle using slow and fast pointers
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            // Cycle detected
+            if (slow == fast) {
+                // Phase 2: Find the entry point of the cycle
+                slow = head;
+                while (slow != fast) {
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                return slow; // Entry point of cycle
             }
-
-            // Mark this node as visited
-            visited.insert(current);
-            current = current->next;
         }
 
-        // If we reach here, there is no cycle
+        // No cycle found
         return nullptr;
-    }
+}
 };
