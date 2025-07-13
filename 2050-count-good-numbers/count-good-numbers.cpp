8c13b94d13ac8f09c302fb8feb7e732ba6bd7e35
Jannat1279 <114873167+Jannat1279@users.noreply.github.com>
2025-07-13 13:26:32 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (69.55%) - LeetSync

---
+++ 2050-count-good-numbers/count-good-numbers.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    const int M = 1e9 + 7;
+    int findPower(long long a, long long b) {
+        // Base case
+        if (b == 0) {
+            return 1;
+        }
+        // Recursive call
+        long long half = findPower(a, b / 2);
+
+        // Square the result and take modulo
+        long long result = (half * half) % M;
+
+        // If the exponent is odd, multiply one more 'a' and take modulo
+        if (b % 2 == 1) {
+            result = (result * a) % M;
+        }
+        return result;
+    }
+    int countGoodNumbers(long long n) {
+        return (long long)findPower(5, (n + 1) / 2) * findPower(4, n / 2)%M;
+    }
+};
