2d6acb9fa5536f045c81f60da09fc08961821f2e
Jannat1279 <114873167+Jannat1279@users.noreply.github.com>
2025-07-14 18:25:53 +0530

    Update longest-consecutive-sequence.cpp

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -31,17 +31,12 @@ st = {1, 2, 3, 4, 100, 200}
 Step : Iterate through the set
 For i = 1
 st.count(0) → false ⇒ valid start of sequence
-
 len = 1
 
 st.count(2) → true ⇒ len = 2
-
 st.count(3) → true ⇒ len = 3
-
 st.count(4) → true ⇒ len = 4
-
 st.count(5) → false ⇒ stop
-
 maxi = max(0, 4) = 4 ✅
 
 For i = 2
@@ -55,15 +50,11 @@ st.count(3) → true ⇒ skip
 
 For i = 100
 st.count(99) → false ⇒ valid start
-
 len = 1, st.count(101) → false ⇒ stop
-
 maxi = max(4, 1) = 4
 
 For i = 200
 st.count(199) → false ⇒ valid start
-
 len = 1, st.count(201) → false ⇒ stop
-
 maxi = max(4, 1) = 4
 */
