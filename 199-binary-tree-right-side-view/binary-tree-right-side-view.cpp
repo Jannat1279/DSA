98804ae09f8a3e16d4a1110c233b75eba4a9df8b
Jannat1279 <114873167+Jannat1279@users.noreply.github.com>
2025-07-16 13:00:19 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (40.55%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -14,14 +14,14 @@ public:
     void preOrder(TreeNode* root, int level, vector<int>& ans){
         if(root==NULL)return;
         // Main step
-        if(ans.size()<level)ans.push_back(root->val);
+        if(ans.size()<=level)ans.push_back(root->val);
         // Recursive call
         preOrder(root->right, level+1, ans);
         preOrder(root->left, level+1, ans);
     }
     vector<int> rightSideView(TreeNode* root) {
         vector<int> ans;
-        preOrder(root,1,ans);
+        preOrder(root,0,ans);
         return ans;
     }
 };
