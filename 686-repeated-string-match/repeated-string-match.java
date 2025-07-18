4d5c1d98db068d2cacd037c4d5517706e2925a5a
Jannat1279 <114873167+Jannat1279@users.noreply.github.com>
2025-07-19 01:32:38 +0530

    Time: 264 ms (37.51%) | Memory: 42.4 MB (68.12%) - LeetSync

---
+++ 686-repeated-string-match/repeated-string-match.java
@@ -0,0 +1,21 @@
+class Solution {
+     public int repeatedStringMatch(String A, String B) {
+        int count = 1;
+        StringBuilder sb = new StringBuilder(A);
+
+        while (sb.length() < B.length()) {
+            sb.append(A);
+            count++;
+        }
+
+        if (sb.toString().contains(B)) {
+            return count;
+        }
+
+        if (sb.append(A).toString().contains(B)) {
+            return count + 1;
+        }
+
+        return -1;
+    }
+}
