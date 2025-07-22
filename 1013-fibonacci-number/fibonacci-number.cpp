64b0e403ac2df8591759be74a41ad212f131a707
Jannat1279 <114873167+Jannat1279@users.noreply.github.com>
2025-07-22 12:04:12 +0530

    Update fibonacci-number.cpp
    With example

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -5,3 +5,21 @@ public:
         return fib(n-1)+fib(n-2);
     }
 };
+
+/*
+🔍 Example:
+fib(4)
+
+🧩 Recursive Tree:
+fib(4)
+= fib(3) + fib(2)
+= (fib(2) + fib(1)) + (fib(1) + fib(0))
+= ((fib(1) + fib(0)) + 1) + (1 + 0)
+= ((1 + 0) + 1) + (1 + 0)
+= (1 + 1) + 1
+= 2 + 1
+= 3
+
+✅ Final Answer:
+fib(4) = 3
+*/
