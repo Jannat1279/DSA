5325ca1c14716f49742d7fe157ba2b5c8751a753
Jannat1279 <114873167+Jannat1279@users.noreply.github.com>
2025-07-24 23:57:24 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (70.31%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,28 +1,12 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        // sum will store the summation of the elements present in the array
-        // i.e. nums
-        int sum = 0;
-        int n = nums.size();
-        for (int i = 0; i < n; i++) {
-            sum = sum + nums[i];
+        int n=nums.size();
+        int sum=0, actualSum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
         }
-        // This is the actual sum claculated using math formula.
-        int actualSum = (n * (n + 1)) / 2;
-        int missingNumber = actualSum - sum;
-        return missingNumber;
+        actualSum=n*(n+1)/2;
+        return actualSum-sum;
     }
 };
-/*
-Example:
-nums=[3, 0, 1]
-for(){
-sum=3 //nums[0]
-sum=3 //nums[1]
-sum=4 //nums[2]
-}
-actualSum=3(3+1)/2=6
-
-actualSum-sum=6-4=2 (answer)
-*/
