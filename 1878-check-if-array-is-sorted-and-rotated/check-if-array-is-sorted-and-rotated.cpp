b124206e8a3bd90be231487d5e9cc37fddfa6ec9
Jannat1279 <114873167+Jannat1279@users.noreply.github.com>
2025-07-24 23:34:21 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (45.25%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -1,24 +1,13 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        int count=0, m=nums.size();
-        for(int i=0;i<m;i++){
-            // allows us to compare the last element to the first, so we can catch the rotation point.
-            if(nums[i]>nums[(i+1)%m]){
+        int n=nums.size();
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]>nums[(i+1)%n]){
                 count++;
             }
         }
         return (count<=1);
     }
 };
-
-/*
-nums[]=[2, 1, 3, 4]
-
-nums[0] > nums[1] → 2 > 1 ✅
-nums[1] > nums[2] → 1 > 3 ❌
-nums[2] > nums[3] → 3 > 4 ❌
-nums[3] > nums[0] → 4 > 2 ✅
-
-count=2, not rotated and sorted.
-*/
