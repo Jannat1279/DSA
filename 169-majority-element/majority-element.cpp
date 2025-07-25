130b0ddb5215310c00d01c5f9465423cc58b3bb3
Jannat1279 <114873167+Jannat1279@users.noreply.github.com>
2025-07-25 13:34:51 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (61.77%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,18 +1,31 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int majority=nums[0],vote=1;
-        for(int i=1;i<nums.size();i++){
-            if(vote==0){
-                vote++;
-                majority=nums[i];
+        int majority =
+            nums[0];  // Assume the first element is the majority candidate
+        int vote = 1; // Initialize vote count for the current candidate
+
+        // Start from the second element
+        for (int i = 1; i < nums.size(); i++) {
+
+            // If current vote count drops to zero, choose a new candidate
+            if (vote == 0) {
+                majority = nums[i];
+                vote = 1;
             }
-            else if(majority==nums[i])vote++;
-            else{
+            // If current number matches the current majority candidate,
+            // increment vote
+            else if (majority == nums[i]) {
+                vote++;
+            }
+            // If current number doesn't match the candidate, decrement vote
+            else {
                 vote--;
             }
         }
+
+        // The element stored in 'majority' is the majority element
+        // (Assuming the problem guarantees that one exists)
         return majority;
     }
-
 };
