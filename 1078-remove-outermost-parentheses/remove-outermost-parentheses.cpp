4057f99607b3ecff0c40fe1ea1729427bfb4c1cd
Jannat1279 <114873167+Jannat1279@users.noreply.github.com>
2025-07-30 13:30:42 +0530

    Time: 3 ms (19.97%) | Memory: 8.8 MB (97.06%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -3,34 +3,13 @@ public:
     string removeOuterParentheses(string s) {
         int count=0;
         string ans="";
-        for(int j=0;j<s.length();j++){
-            if(s[j]==')')count--;
-            if(count!=0)ans.push_back(s[j]);
-            if(s[j]=='(')count++;
+        for(char ch:s){
+            if(ch==')')count--;
+            if(count!=0)ans.push_back(ch);
+            if(ch=='(')count++;
         }
         return ans;
     }
+
+   
 };
-/*
- How It Works : Step-by-step on input "(()())(())"
-
-| Char | count before | Action                  | ans    | count after |
-| ---- | ------------ | ----------------------- | ------ | ----------- |
-| (    | 0            | skip (count != 0 false) |        | 1           |
-| (    | 1            | add '('                 | (      | 2           |
-| )    | 2            | add ')'                 | ()     | 1           |
-| (    | 1            | add '('                 | ()(    | 2           |
-| )    | 2            | add ')'                 | ()()   | 1           |
-| )    | 1            | skip (count becomes 0)  | ()()   | 0           |
-| (    | 0            | skip                    | ()()   | 1           |
-| (    | 1            | add '('                 | ()()(  | 2           |
-| )    | 2            | add ')'                 | ()()() | 1           |
-| )    | 1            | skip                    | ()()() | 0           |
-
-When you see ')', you're closing a level. If this ')' brings the count back to 0, then it is the outermost closing bracket, and you should not include it in ans.
-If you had added ')' to ans before decreasing count, you'd be mistakenly adding the outermost closing parentheses.
-
-Time: O(n) where n is the length of string s
-Space: O(n) for storing the answer string
-
-*/
