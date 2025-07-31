7d7e7ee41fd31bc807b5f4daf53b7775c54ab2b4
Jannat1279 <114873167+Jannat1279@users.noreply.github.com>
2025-07-31 13:30:40 +0530

    Time: 19 ms (48.74%) | Memory: 26 MB (47.93%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        // Hash map to group anagrams by their sorted string
+        // Key: sorted version of the string
+        // Value: list of original strings that are anagrams
+        unordered_map<string, vector<string>> mp;
+
+        // Loop through each string in the input
+        for (auto str : strs) {
+            string temp = str;
+
+            // Sort the string to get the canonical form
+            // All anagrams will have the same sorted form
+            sort(temp.begin(), temp.end());
+
+            // Add the original string to the vector mapped by the sorted string
+            mp[temp].push_back(str);
+        }
+
+        // Prepare the final result by collecting all groups of anagrams
+        vector<vector<string>> result;
+        for (auto it : mp) {
+            result.push_back(
+                it.second); // it.second is a vector of grouped anagrams
+        }
+
+        // Return all grouped anagrams
+        return result;
+    }
+};
