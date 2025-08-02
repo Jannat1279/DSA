880d847bdbb00118a4ffe3a13609c9faa2de5ce5
Jannat1279 <114873167+Jannat1279@users.noreply.github.com>
2025-08-03 00:10:27 +0530

    Update build-an-array-with-stack-operations.cpp

--- 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.cpp
+++ 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.cpp
@@ -10,8 +10,7 @@ public:
         while (i < target.size() && stream <= n) {
             result.push_back("Push"); // Always push the current number (stream)
 
-            if (stream ==
-                target[i]) { // If the current number matches target element
+            if (stream == target[i]) { // If the current number matches target element
                 i++;         // Move to next target element
             } else {
                 result.push_back("Pop"); // Otherwise, pop it immediately since
