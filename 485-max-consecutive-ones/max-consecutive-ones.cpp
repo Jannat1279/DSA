fa5424ea593b6e4f1ad62fa45dac1a86a2bc98d4
Jannat1279 <114873167+Jannat1279@users.noreply.github.com>
2025-08-04 17:26:55 +0530

    Update max-consecutive-ones.cpp
    With example

--- 485-max-consecutive-ones/max-consecutive-ones.cpp
+++ 485-max-consecutive-ones/max-consecutive-ones.cpp
@@ -14,3 +14,20 @@ public:
         return max(maxCount, count);
     }
 };
+
+/*
+Example:
+vector<int> nums = {1, 1, 0, 1, 1, 1};
+
+| Index | nums\[i] | count | maxCount |
+| ----- | -------- | ----- | -------- |
+| 0     | 1        | 1     | 0        |
+| 1     | 1        | 2     | 0        |
+| 2     | 0        | 0     | 2        |
+| 3     | 1        | 1     | 2        |
+| 4     | 1        | 2     | 2        |
+| 5     | 1        | 3     | 2        |
+
+return max(3, 2) = 3
+
+*/
