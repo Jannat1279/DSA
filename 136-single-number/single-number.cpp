4418064804c1ae94dea372a63ff64a1e4d2b9b11
Jannat1279 <114873167+Jannat1279@users.noreply.github.com>
2025-08-15 19:54:07 +0530

    Update single-number.cpp

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,10 +1,18 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int res=0;
-        for(int num:nums){
-            res=res^num;
+        int res = 0; // Variable to store the result
+        
+        // Loop through all elements in the array
+        for (int num : nums) {
+            // XOR operation:
+            // - If two numbers are the same, XOR will make them 0 (num ^ num = 0)
+            // - 0 ^ num = num
+            // - Since all elements except one appear twice, pairs will cancel out
+            // - The remaining single element will be stored in 'res'
+            res = res ^ num;
         }
-        return res;
+        
+        return res; // Return the element that appears only once
     }
 };
