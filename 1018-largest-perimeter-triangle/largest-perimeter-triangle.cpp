eeef9d29797fd21153711bc1fc918f4a85dd72e8
Jannat1279 <114873167+Jannat1279@users.noreply.github.com>
2025-08-16 18:46:34 +0530

    Update largest-perimeter-triangle.cpp

--- 1018-largest-perimeter-triangle/largest-perimeter-triangle.cpp
+++ 1018-largest-perimeter-triangle/largest-perimeter-triangle.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
     int largestPerimeter(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int n=nums.size();
-        for(int i=n-3;i>=0;i--){
-            // Sum of two smaller sides should be greater than the third to form a triangle
-            if(nums[i]+nums[i+1]>nums[i+2]){
-                return nums[i]+nums[i+1]+nums[i+2];
+        // Step 1: Sort the array so sides are in non-decreasing order
+        sort(nums.begin(), nums.end());
+        
+        int n = nums.size();
+        
+        // Step 2: Iterate from the largest side towards the front
+        // We check triplets (nums[i], nums[i+1], nums[i+2]) in descending order
+        for (int i = n - 3; i >= 0; i--) {
+            // Triangle inequality condition:
+            // Sum of two smaller sides must be greater than the largest side
+            if (nums[i] + nums[i + 1] > nums[i + 2]) {
+                // If valid, return the perimeter of this triangle
+                return nums[i] + nums[i + 1] + nums[i + 2];
             }
         }
+        
+        // Step 3: If no valid triangle can be formed, return 0
         return 0;
     }
 };
