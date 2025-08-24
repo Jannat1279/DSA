17593821fe4c5575bd6cc0eb1b87c56e28173eb3
Jannat1279 <114873167+Jannat1279@users.noreply.github.com>
2025-08-24 15:53:19 +0530

    Update reverse-string.cpp

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        int n=s.size();
-        int st=0;
-        int e=n-1;
-        while(st<e){
-            swap(s[st++],s[e--]);
+        int n = s.size();   // Get the size of the string (vector of characters)
+        int st = 0;         // Start pointer (points to the beginning of the string)
+        int e = n - 1;      // End pointer (points to the last character of the string)
+        
+        // Keep swapping characters until start crosses end
+        while (st < e) {
+            swap(s[st++], s[e--]);  
+            // swap characters at st and e, 
+            // then increment st and decrement e
         }
     }
 };
+
