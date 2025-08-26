8eef1f7354a45f15e4efc79e5f48c0642a9a7901
Jannat1279 <114873167+Jannat1279@users.noreply.github.com>
2025-08-27 00:13:56 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (47.90%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -1,14 +1,26 @@
+// class Solution {
+// public:
+//     bool isPowerOfThree(int n) {
+//         if(n==1)return true;
+//         while(n>0){
+//             if(n%3!=0)return false;
+//             if(n==3)return true;
+//             else{
+//                 n/=3;
+//             }
+//         }
+//         return false;
+//     }
+// };
+
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if(n==1)return true;
-        while(n>0){
-            if(n%3!=0)return false;
-            if(n==3)return true;
-            else{
-                n/=3;
-            }
-        }
-        return false;
+        // Base cases
+        if (n == 1) return true;       // 3^0 = 1
+        if (n <= 0 || n % 3 != 0) return false;
+
+        // Recursive case
+        return isPowerOfThree(n / 3);
     }
 };
