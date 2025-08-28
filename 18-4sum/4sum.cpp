922656aea4e050d0bb3cd6fd7ed70481255f9f54
Jannat1279 <114873167+Jannat1279@users.noreply.github.com>
2025-08-28 12:32:15 +0530

    Time: 197 ms (9.89%) | Memory: 53.4 MB (11.75%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,37 +1,41 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        int n=nums.size();
+        int n = nums.size();
+        set<vector<int>> uniqueQuadruplets; // to avoid duplicates
         vector<vector<int>> ans;
-        sort(nums.begin(), nums.end());
-        for(int i=0;i<n;i++){
-            // duplicate at the beginning
-            if(i>0 && nums[i]==nums[i-1])continue;
-            for(int j=i+1;j<n;j++){
-                // Duplicate but not at the beginning
-                if(j!=i+1 && nums[j]==nums[j-1])continue;
-                int k=j+1;
-                int l=n-1;
-                while(k<l){
-                    long long sum=nums[i];
-                    sum+=nums[j];
-                    sum+=nums[k];
-                    sum+=nums[l];
-                    if(sum==target){
-                        vector<int> temp={nums[i], nums[j], nums[k], nums[l]};
-                        ans.push_back(temp);
-                        k++; l--;
-                        // For duplicates
-                        while(k<l && nums[k]==nums[k-1])k++;
-                        while(k<l && nums[l]==nums[l+1])l--;
+
+        sort(nums.begin(), nums.end()); // sort for two-pointer method
+
+        // Pick first element
+        for (int i = 0; i < n - 3; i++) {
+            // Pick second element
+            for (int j = i + 1; j < n - 2; j++) {
+                int k = j + 1, l = n - 1;
+
+                while (k < l) {
+                    long long sum = (long long)nums[i] + nums[j] + nums[k] + nums[l];
+
+                    if (sum == target) {
+                        uniqueQuadruplets.insert({nums[i], nums[j], nums[k], nums[l]});
+                        k++;
+                        l--;
+                    } 
+                    else if (sum < target) {
+                        k++;
+                    } 
+                    else {
+                        l--;
                     }
-                    else if(sum<target)k++;
-                    else{
-                        l--;}
-                    
                 }
             }
         }
+
+        // Move unique quadruplets from set to answer
+        for (auto &quad : uniqueQuadruplets) {
+            ans.push_back(quad);
+        }
+
         return ans;
     }
 };
