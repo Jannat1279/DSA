a720fb5f59381c46893d3836bc38bb308a0ba809
Jannat1279 <114873167+Jannat1279@users.noreply.github.com>
2025-09-09 23:53:27 +0530

    Update symmetric-tree.cpp

--- 101-symmetric-tree/symmetric-tree.cpp
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -1,14 +1,3 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     bool check(TreeNode* l, TreeNode* r){
@@ -41,7 +30,6 @@ check(root->left, root->right)
 2 == 2 ✅ → check outer and inner pairs:
 
 check(3, 3) → ✅
-
 check(4, 4) → ✅
 
 All pairs matched → return true
