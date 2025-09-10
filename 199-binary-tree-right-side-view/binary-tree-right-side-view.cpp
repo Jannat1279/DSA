3960ac329f0414308ea425919fbccf3a37171d6c
Jannat1279 <114873167+Jannat1279@users.noreply.github.com>
2025-09-10 11:35:51 +0530

    Update binary-tree-right-side-view.cpp

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -1,30 +1,30 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    void preOrder(TreeNode* root, int level, vector<int>& ans){
-        if(root==NULL)return;
-        // Main step
-        if(ans.size()<=level)ans.push_back(root->val);
-        // Recursive call
-        preOrder(root->right, level+1, ans);
-        preOrder(root->left, level+1, ans);
-    }
     vector<int> rightSideView(TreeNode* root) {
         vector<int> ans;
-        preOrder(root,0,ans);
+        if(!root) return ans;
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        while(!q.empty()){
+            int size = q.size();
+            for(int i = 0; i < size; i++){
+                TreeNode* node = q.front();
+                q.pop();
+                
+                // If it's the last node of this level -> visible from right
+                if(i == size - 1) ans.push_back(node->val);
+                
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+            }
+        }
+        
         return ans;
     }
 };
+
 
 /*
 Example:
