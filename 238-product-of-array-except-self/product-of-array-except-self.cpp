30679501d8be3d38d83707642221858bfe4319b9
Jannat <114873167+Jannat1279@users.noreply.github.com>
2025-09-26 13:02:59 +0530

    Time: 4 ms (21.62%) | Memory: 40.2 MB (79.23%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> result(n, 1);
+
+        // Step 1: prefix pass
+        // result[i] will store product of all elements to the left of i
+        int prefix = 1;
+        for (int i = 0; i < n; i++) {
+            result[i] = prefix;
+            prefix *= nums[i];
+        }
+
+        // Step 2: suffix pass
+        // multiply with product of all elements to the right of i
+        int suffix = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            result[i] *= suffix;
+            suffix *= nums[i];
+        }
+
+        return result;
+    }
+};
