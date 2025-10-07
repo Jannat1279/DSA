da9a2ffd9b93b17ad90152bd6e67aca71c226581
Jannat <114873167+Jannat1279@users.noreply.github.com>
2025-10-07 11:14:14 +0530

    Update reverse-linked-list.cpp

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -1,13 +1,6 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+// time - O(n), space - O(1)
+
+
 class Solution {
 public:
     // Function to reverse a singly linked list
