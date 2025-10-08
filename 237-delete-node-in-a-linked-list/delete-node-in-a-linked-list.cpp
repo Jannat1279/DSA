c0581f979859d807a91925174f5ed895940b8bd8
Jannat <114873167+Jannat1279@users.noreply.github.com>
2025-10-08 12:03:49 +0530

    Update delete-node-in-a-linked-list.cpp

--- 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
@@ -1,11 +1,5 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
+// time - O(1), space - O(1)
+
 class Solution {
 public:
     void deleteNode(ListNode* node) {
